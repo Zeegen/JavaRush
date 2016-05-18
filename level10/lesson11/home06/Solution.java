4124ebe9505ad1833b455cffda245837fb90a0a5
Zeegen <dogy@Nikolajs-MacBook-Pro.local>
2016-05-18 19:18:47 +0300

    level up

--- level10/lesson11/home06/Solution.java
+++ level10/lesson11/home06/Solution.java
@@ -1,11 +1,8 @@
 package com.javarush.test.level10.lesson11.home06;
 
 /* Конструкторы класса Human
-Напиши класс Human с 6 полями. Придумай и реализуй 10 различных конструкторов для него.
-Каждый конструктор должен иметь смысл.
+Напиши класс Human с 6 полями. Придумай и реализуй 10 различных конструкторов для него. Каждый конструктор должен иметь смысл.
 */
-
-import sun.text.normalizer.SymbolTable;
 
 public class Solution
 {
@@ -16,16 +13,87 @@ public class Solution
 
     public static class Human
     {
-        String name;
-        int age;
-        int weight;
-        int height;
-        boolean sex;
-        String color;
-        public Human(String name){
-            this.name = name;
-        }
 
-        //напишите тут ваши переменные и конструкторы
+            private String name;
+            private String surname;
+            private int age;
+            private String address;
+            private int phone;
+            private String car;
+
+            public Human(String name)
+            {
+                this.name = name;
+            }
+
+            public Human(String name, String surname)
+            {
+                this.name = name;
+                this.surname = surname;
+            }
+
+            public Human(String name, String surname, int age)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.age = age;
+            }
+
+            public Human(String name, String surname, int age, String address)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.age = age;
+                this.address = address;
+            }
+
+            public Human(String name, String surname, int age, String address, int phone)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.age = age;
+                this.address = address;
+                this.phone = phone;
+            }
+
+            public Human(String name, String surname, int age, String address, int phone, String car)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.age = age;
+                this.address = address;
+                this.phone = phone;
+                this.car = car;
+            }
+
+            public Human(String surname, int age)
+            {
+                this.surname = surname;
+                this.age = age;
+            }
+
+            public Human(String surname, int age, String address)
+            {
+                this.surname = surname;
+                this.age = age;
+                this.address = address;
+            }
+
+            public Human(String surname, int age, String address, int phone)
+            {
+                this.surname = surname;
+                this.age = age;
+                this.address = address;
+                this.phone = phone;
+            }
+
+            public Human(String surname, int age, String address, int phone, String car)
+            {
+                this.surname = surname;
+                this.age = age;
+                this.address = address;
+                this.phone = phone;
+                this.car = car;
+            }//напишите тут ваши переменные и конструкторы
     }
 }
